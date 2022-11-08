e5b1acc7fba784bc30c53c9a0680e32009ad299f
Emma-Onye <eonyebuashi@gmail.com>
2022-11-07 22:06:45 -0500

    first commit of infra

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web" {
+  ami           = var.AMI_ID
+  instance_type = var.INSTANCE_TYPE
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+
+
+
+
+
+
+# resource "aws_instance" "web" {
+#   ami           = var.AMI_ID
+#   instance_type = var.INSTANCE_TYPE
+#   monitoring = true
+#   ebs_optimized = true
+
+#   root_block_device {
+#   encrypted     = true
+#   }
+
+#   tags = {
+#     Name = "airbnb-web-server-instance"
+#   }
+# }
